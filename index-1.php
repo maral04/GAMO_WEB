e2fbe4231e42116b563b94ec41e519232898c197
arnauhortelano <arnau.hortelano@gmail.com>
2016-05-02 11:54:15 +0200

    Millora api get proves i rebre les dades del json

--- index-1.php
+++ index-1.php
@@ -5,17 +5,46 @@
     ?>
     <title>GAMO: Event List</title>
     <script>
-        $(".accordion").on("click", function()
-        {
-            var id = $(this).attr('eventid');
-            $.ajax({
-                dataType: "json",
-                url: "http://localhost/GAMO_WEB/api/events/getProves.php?eventId="+id,
-                success: function(data) {
-                    console.log((data));
-                }
+        $(document).ready(function(){
+            $(".accordion").on("click", function()
+            {
+                var id = $(this).attr('eventid');
+                $.ajax({
+                    // la URL para la petición
+                    url : 'http://localhost/GAMO_WEB/api/events/getProves.php',
+
+                    // la información a enviar
+                    // (también es posible utilizar una cadena de datos)
+                    data : { eventId : id },
+
+                    // especifica si será una petición POST o GET
+                    type : 'GET',
+
+                    // el tipo de información que se espera de respuesta
+                    dataType : 'json',
+
+                    // código a ejecutar si la petición es satisfactoria;
+                    // la respuesta es pasada como argumento a la función
+                    success : function(json) {
+                        console.log(json);
+                    },
+
+                    // código a ejecutar si la petición falla;
+                    // son pasados como argumentos a la función
+                    // el objeto de la petición en crudo y código de estatus de la petición
+                    error: function (request, status, error) {
+                        alert(request.responseText);
+                        console.log(error);
+                    },
+
+                    // código a ejecutar sin importar si la petición falló o no
+                    complete : function(xhr, status) {
+                        alert('Petición realizada');
+                    }
+                });
             });
         });
+
     </script>
 </head>
 <body class="" id="top">
