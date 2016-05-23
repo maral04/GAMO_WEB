00563aca5858bfd8a00584f143bd819625b6db70
maral04 <uslo112@gmail.com>
2016-05-23 09:52:44 +0200

    WIP llistatInscrits

--- llistatInscrits.php
+++ llistatInscrits.php
@@ -1,52 +1,59 @@
 <head>
-<?php
-/**
- * Created by PhpStorm.
- * User: Arnau
- * Date: 22/05/2016
- * Time: 17:14
- */
+    <?php
 
-include_once "classes/DataBase.php";
-include_once "head.php";
-include_once "classes/DataBase.php";
+    include_once "head.php";
+    include_once "classes/DataBase.php";
 
-$db = new DataBase();
-$conn = $db->connect();
-?>
+    $db = new DataBase();
+    $conn = $db->connect();
+    ?>
 </head>
 <body>
 <div class="main">
-
     <?php include_once "header.php"; ?>
 
-    <div class="container_12">
-        <div class="block3 grid_12"
+    <div class="content container_12" >
         <?php
+        if (isset($_GET['idProva'])) {
+            $idProva = $_GET['idProva'];
+            $sql = "SELECT prova.nom as nomProva, usuari.nom, usuari.cNom, (SELECT nom FROM club WHERE usuari.FK_id_club = club.Id) as nomClub FROM inscripcio INNER JOIN prova on Fk_Id_prova = prova.Id INNER JOIN usuari on id_Participant = usuari.id  WHERE prova.Id =" . $idProva;
 
-            if(isset($_GET['idProva']) ) {
-                $idProva = $_GET['idProva'];
-                $sql = "SELECT prova.nom as nomProva, usuari.nom, usuari.cNom, (SELECT nom FROM club WHERE usuari.FK_id_club = club.Id) as nomClub FROM inscripcio INNER JOIN prova on Fk_Id_prova = prova.Id INNER JOIN usuari on id_Participant = usuari.id  WHERE prova.Id =" . $idProva;
+            $result = $db->getConn()->query($sql);
 
-                $result = $db->getConn()->query($sql);
-
+            echo "<div class='grid_12'>";
+                echo "<h3 class='registre h3__head1'></h3>";
+            echo "</div>";
+            echo "<div class='grid_11 block3'>";
                 if ($result->num_rows > 0) {
-                    echo "<table style='border:1px solid '>";
-                    echo "<tr><th>Name</th><th>Lastname</th><th>Sex</th><th>Club</th></tr>";
-
+                    echo "<table class='taulaintro'>";
+                    echo "<tr>
+                        <th>Name</th>
+                        <th>Last Name</th>
+                        <th>Gender</th>
+                        <th>Club</th></tr>
+                        ";
+                    $i = 0;
                     while ($arrayEvent = mysqli_fetch_assoc($result)) {
-
-                        echo "<tr><td>".$arrayEvent['nom']."</td><td>".$arrayEvent['cNom']."</td><td></td><td>".$arrayEvent['nomClub']."</td></tr>";
+                    if($i == 0){
+                        $i=1;
+                        ?>
+                        <script>
+                            $( document ).ready(function() {
+                                $('.registre').text("List of Participants in <?php echo $arrayEvent['nomProva'] ?>");
+                            });
+                        </script>
+                        <?php
+                    }
+                        echo "<tr><td>" . $arrayEvent['nom'] . "</td><td>" . $arrayEvent['cNom'] . "</td><td></td><td>" . $arrayEvent['nomClub'] . "</td></tr>";
                     }
                     echo "</table>";
                 }
-            }else{
+            } else {
                 header("Location: index.php");
             }
-
-        ?>
+            ?>
         </div>
-        </div>
+    </div>
 <div class="clear"></div>
 </div>
 </body>
